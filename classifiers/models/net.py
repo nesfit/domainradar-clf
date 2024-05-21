a201c9269c83a7d83c967e948b8231aa2f5eedfe
survivecz <poucpetr11@seznam.cz>
2024-05-21 20:31:19 +0200

    seriózní commit
    opravdu seriózní commit, který nepůsobí amatérsky

---
+++ classifiers/models/net.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self, side_size):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        
+        self.fc1 = nn.Linear(128 * (side_size-6)**2, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 128)
+        self.fc4 = nn.Linear(128, 2)
+        
+        self.dropout1 = nn.Dropout(0.3)
+        self.dropout2 = nn.Dropout(0.3)
+        
+        print("CNN model created")
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        
+        x = torch.flatten(x, 1)
+        x = self.dropout1(x)
+        
+        x = F.relu(self.fc1(x))
+        x = self.dropout2(x)
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        return self.fc4(x)
--- classifiers/preprocessor.py
+++ classifiers/preprocessor.py
@@ -28,9 +28,9 @@ class Preprocessor:
         self.stored = {'dga': dict(), 'phishing': dict(), 'malware': dict()}
         self.stored["dga"]["scaler"] = joblib.load('boundaries/dga_scaler.joblib')
         self.stored["dga"]["outliers"] = joblib.load('boundaries/dga_outliers.joblib')
-        self.stored["phishing"]["scaler"] = joblib.load('boundaries/phishing_scaler.joblib')
-        self.stored["phishing"]["outliers"] = joblib.load('boundaries/phishing_outliers.joblib')
-        self.stored["phishing"]["cf_model"] = joblib.load('models/phishing_decision_tree_model.joblib')
+        self.stored["phishing"]["scaler"] = joblib.load('boundaries/scaler_benign2312misp2402.joblib')
+        self.stored["phishing"]["outliers"] = joblib.load('boundaries/outliers_benign2312misp2402.joblib')
+        self.stored["phishing"]["cf_model"] = joblib.load('models/decision_tree_benign2312phishing2402.joblib')
         self.stored["malware"]["scaler"] = joblib.load('boundaries/malware_scaler.joblib')
         self.stored["malware"]["outliers"] = joblib.load('boundaries/malware_outliers.joblib')
         self.stored["malware"]["cf_model"] = joblib.load('models/malware_decision_tree_model.joblib')
