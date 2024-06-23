3dff81ca1e4cce2f313ac78b3274693348c14b54
Jan Polisensky <Polisensky.john@gmail.com>
2024-06-23 17:50:02 +0200

    sigmoid

--- classifiers/Clf_phishing_cnn.py
+++ classifiers/Clf_phishing_cnn.py
@@ -144,6 +144,6 @@ class Clf_phishing_cnn:
 
         with torch.no_grad():
             outputs = self.model(data_tensor_reshaped)
-            outputs_cpu = outputs.cpu().numpy()
+            outputs_cpu = torch.sigmoid(outputs).round().cpu().numpy()
 
         return outputs_cpu
